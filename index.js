5a2912305bacbf0c637023055e7f26b1422efdc0
Grant Robinson <Chisdeal2014@hotmail.com>
2022-01-23 12:31:38 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 #!/usr/bin/env node
 'use strict'
 
-const { create, urlSource } = require('ipfs-http-client')
+const { create, urlSource, ipfsHttpClient } = require('ipfs-http-client')
 const [ url ] = process.argv.slice(2)
 
-const ipfs = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
+const ipfs = create('https://ipfs.infura.io:5001/api/v0')
 
 if (!url) {
   console.error('expected a url as argument')
   process.exit(1)
 }
 
-ipfs.add(urlSource(url))
-.then(res => res[0] && res[0].cid)
+async function UrlToIPFS(url) {
+
+const file = await ipfs.add(urlSource(url));
+
+var cid = 'https://ipfs.infura.io/ipfs/'+file.cid;
+
+return {'path': file.path, 'cid': file.cid, 'size': file.size, 'url': cid};
+
+}
+
+UrlToIPFS(url)
+.then(res => res)
 .then(console.log)
 .catch(console.error)
